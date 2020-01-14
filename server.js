1bf60836b4e328067a7c6693b434dafdbfc48c1b
Arman Riahi <namrataffy@gmail.com>
2020-01-13 18:13:51 -0800

    trying api routes

--- server.js
+++ server.js
@@ -12,6 +12,26 @@ app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+  let theJSON = fs.readFile("./db/db.json", function(err, data) {
+    if (err) return console.error(err);
+    let X = JSON.parse(data);
+    console.log(X);
+    return X;
+  });
+  console.log(theJSON);
+  res.json(theJSON);
+});
+
+app.post("/api/notes", function(req, res) {
+  var newNote = req.body;
+  console.log(newNote);
+});
+
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
 });
